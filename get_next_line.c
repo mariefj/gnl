daf2651e33eaa516bc3f912db01b1ea29a762d90
mariefj <marie.forja@gmail.com>
2022-01-12 15:08:12 +0100

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,7 @@
+#include "my.h"
+#include "get_next_line.h"
+
+char *get_next_line(const int fd)
+{
+
+}
---
+++ get_next_line.h
@@ -0,0 +1,11 @@
+#ifndef _GET_NEXT_LINE_H_
+#define _GET_NEXT_LINE_H_
+
+#ifndef READ_SIZE
+#define READ_SIZE = 150
+
+#endif
+
+char *get_next_line(const int fd);
+
+#endif // _GET_NEXT_LINE_H_
