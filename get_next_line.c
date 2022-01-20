6fc5200339d61042662f8bc9df0d8160560f61de
mariefj <marie.forja@gmail.com>
2022-01-20 13:19:08 +0100

    ...

--- get_next_line.c
+++ get_next_line.c
@@ -45,81 +45,89 @@ static char	*my_append_c(char *str, char c)
 	return (new_str);
 }
 
-static char	*str_merge(char const *dest, char const *src, int size)
-{
-	int i = 0;
-	int j = 0;
-	int len = (dest == NULL ? 0 : my_strlen(dest)) + (src == NULL ? 0 : size);
-	char *new = (char *)malloc((len + 1) * sizeof(char));
+// static char	*str_merge(char const *dest, char const *src, int size)
+// {
+// 	int i = 0;
+// 	int j = 0;
+// 	int len = (dest == NULL ? 0 : my_strlen(dest)) + (src == NULL ? 0 : size);
+// 	char *new = (char *)malloc((len + 1) * sizeof(char));
 
-	while (dest != NULL && dest[i] != '\0')
-	{
-		new[i] = dest[i];
-		i++;
-	}
-	while (src != NULL && j < size)
-	{
-		new[i] = src[j];
-		i++;
-		j++;
-	}
-	new[i] = '\0';
+// 	while (dest != NULL && dest[i] != '\0')
+// 	{
+// 		new[i] = dest[i];
+// 		i++;
+// 	}
+// 	while (src != NULL && j < size)
+// 	{
+// 		new[i] = src[j];
+// 		i++;
+// 		j++;
+// 	}
+// 	new[i] = '\0';
 
-	return (new);
-}
+// 	return (new);
+// }
 
-static char	*get_str_end_file(char *content, int count)
-{
-	int i = 0;
-	char *str = (char *)malloc((count + 1) * sizeof(char));
+// static char	*get_content_to_str(int fd)
+// {
+// 	int count = READ_SIZE;
+// 	char content[READ_SIZE];
+// 	char *str = NULL;
 
-	while (i < count)
-	{
-		str[i] = content[i];
-		i++;
-	}
-	str[i] = '\0';
+// 	while (count != 0)
+// 	{
+// 		count = read(fd, content, READ_SIZE);
+// 		if (count == -1)
+// 			return (NULL);
 
-	return (str);
-}
+// 		if (count == READ_SIZE)
+// 			str = str_merge(str, content, count);
+// 	}
 
-static char	*get_content_to_str(int fd)
-{
-	int count = READ_SIZE;
-	char content[READ_SIZE];
-	char *str = NULL;
-
-	while (count != 0)
-	{
-		count = read(fd, content, READ_SIZE);
-		if (count == -1)
-			return (NULL);
-
-		if (count == READ_SIZE)
-			str = str_merge(str, content, count);
-		else
-			str = str_merge(str, get_str_end_file(content, count), count);
-	}
-
-	return (str);
-}
+// 	return (str);
+// }
 
 char	*get_next_line(const int fd)
 {
-	static char *str = NULL;
-	int size_lign = 0;
+	static char str[READ_SIZE];
+	int count = READ_SIZE;
+	int size = 0;
 	char *lign = NULL;
 
 	if (str == NULL)
 	{
-		str = get_content_to_str(fd);
+		count = read(fd, str, READ_SIZE);
+		if (count == -1)
+			return (NULL);
 	}
-	while (str[size_lign] != '\n' && str[size_lign] != '\0')
+	while (*str != '\n' && *str != '\0')
 	{
-		lign = my_append_c(lign, str[size_lign]);
-		size_lign++;
+		lign = my_append_c(lign, str[size]);
+		str++;
+		size++;
 	}
-	str += (size_lign + 1);
+	if (size == READ_SIZE - 1)
+		return (NULL);
 
-	return (lign);
+	return(lign);
 }
+
+// char	*get_next_line(const int fd)
+// {
+// 	static char *str = NULL;
+// 	int size_lign = 0;
+// 	char *lign = NULL;
+
+// 	if (str == NULL)
+// 	{
+// 		str = get_content_to_str(fd);
+// 	}
+// 	while (str[size_lign] != '\n' && str[size_lign] != '\0')
+// 	{
+// 		lign = my_append_c(lign, str[size_lign]);
+// 		size_lign++;
+// 	}
+// 	str += (size_lign + 1);
+
+// 	return (lign);
+// }
