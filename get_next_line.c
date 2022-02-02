728168f5a3689dc03c566257381a6819b4d7a6dd
mariefj <marie.forja@gmail.com>
2022-02-02 21:53:50 +0100

    gnl with libc

--- get_next_line.c
+++ get_next_line.c
@@ -1,93 +1,30 @@
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 
 #include "get_next_line.h"
 
-static size_t	my_strlen(char *str)
-{
-	int i = 0;
-
-	if (str == NULL)
-		return (0);
-
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-static void	*my_memcpy(void *dest, const void *src, size_t n)
-{
-	char *dest_str;
-	const char *src_str;
-	long *dest_long = dest;
-	const long *src_long = src;
-
-	while (n >= sizeof(long) * 4)
-	{
-		n -= sizeof(long) * 4;
-		*dest_long++ = *src_long++;
-		*dest_long++ = *src_long++;
-		*dest_long++ = *src_long++;
-		*dest_long++ = *src_long++;
-	}
-	while (n >= sizeof(long))
-	{
-		n -= sizeof(long);
-		*dest_long++ = *src_long++;
-	}
-	dest_str = (char *)dest_long;
-	src_str = (const char *)src_long;
-	while (n--)
-	{
-		*dest_str++ = *src_str++;
-	}
-
-	return (dest);
-}
-
-static void	*my_memmove(void *dest, const void *src, size_t n)
-{
-	char *dest_str = (char *)dest;
-	char *src_str = (char *)src;
-	long *dest_long = (long *)dest;
-	long *src_long = (long *)src;
-
-	if (dest > src)
-	{
-		while (n % sizeof(long) != 0)
-		{
-			n--;
-			dest_str[n] = src_str[n];
-		}
-		for (size_t i = n / sizeof(long); i > 0; i--)
-		{
-			dest_long[i - 1] = src_long[i - 1];
-		}
-	}
-	else
-		my_memcpy(dest, src, n);
-
-	return (dest);
-}
-
 static char	*str_merge(char *dest, char *src)
 {
-	int dest_len = my_strlen(dest);
-	int src_len = my_strlen(src);
+	int dest_len;
+	int src_len;
 	char *new;
 
+	if (dest == NULL)
+		return (NULL);
+
+	if (src == NULL)
+		return (dest);
+
+	dest_len = strlen(dest);
+	src_len = strlen(src);
 	new = malloc((dest_len + src_len + 1) * sizeof(char));
 	if (new == NULL)
 		return (NULL);
 
-	my_memcpy(new, dest, dest_len);
-	my_memcpy(new + dest_len, src, src_len);
-
+	memcpy(new, dest, dest_len);
+	memcpy(new + dest_len, src, src_len);
 	new[dest_len + src_len] = '\0';
 	free(dest);
 
@@ -117,7 +54,7 @@ static char	*fill_str(char *src, int pos)
 	if (str == NULL)
 		return (NULL);
 
-	my_memcpy(str, src, pos);
+	memcpy(str, src, pos);
 	str[pos] = '\0';
 
 	return (str);
@@ -125,20 +62,20 @@ static char	*fill_str(char *src, int pos)
 
 static void	fill_buff(char *buff, char *src, int size, int length)
 {
-	my_memcpy(buff, src, size);
+	memcpy(buff, src, size);
 	buff[length] = '\0';
 }
 
 static char	*case_buff_not_empty_with_line_feed(char *buff, int pos)
 {
 	char *str;
-	size_t buff_len = my_strlen(buff);
+	size_t buff_len = strlen(buff);
 
 	str = fill_str(buff, pos);
 	if (str == NULL)
 		return (NULL);
 
-	my_memmove(buff, buff + pos + 1, buff_len - pos);
+	memmove(buff, buff + pos + 1, buff_len - pos);
 	buff[buff_len - (pos + 1)] = '\0';
 
 	return (str);
@@ -186,7 +123,7 @@ static char	*case_buff_empty(char *buff, int fd)
 
 static char	*case_stdin_with_line_feed(char *buff, char *stream, int pos_stream, int readchar)
 {
-	size_t buff_len = my_strlen(buff);
+	size_t buff_len = strlen(buff);
 	char *str;
 	char *src = stream + pos_stream + 1;
 	int size = readchar - pos_stream;
@@ -195,8 +132,8 @@ static char	*case_stdin_with_line_feed(char *buff, char *stream, int pos_stream,
 	if (str == NULL)
 		return (NULL);
 
-	my_memcpy(str, buff, buff_len);
-	my_memcpy(str + buff_len, stream, pos_stream);
+	memcpy(str, buff, buff_len);
+	memcpy(str + buff_len, stream, pos_stream);
 	fill_buff(buff, src, size, readchar - (pos_stream + 1));
 	str[buff_len + pos_stream] = '\0';
 
@@ -205,7 +142,7 @@ static char	*case_stdin_with_line_feed(char *buff, char *stream, int pos_stream,
 
 static char	*case_stdin_without_line_feed(char *buff, char *stream, int readchar, int fd)
 {
-	size_t buff_len = my_strlen(buff);
+	size_t buff_len = strlen(buff);
 	char *str_rec;
 	char *str;
 
@@ -213,7 +150,7 @@ static char	*case_stdin_without_line_feed(char *buff, char *stream, int readchar
 	if (str == NULL)
 		return (NULL);
 
-	my_memcpy(str, buff, buff_len);
+	memcpy(str, buff, buff_len);
 	str[buff_len] = '\0';
 
 	buff[0] = '\0';
@@ -250,7 +187,7 @@ static char	*case_buff_not_empty(char *buff, int fd)
 {
 	char stream[READ_SIZE + 1];
 	int readchar;
-	size_t buff_len = my_strlen(buff);
+	size_t buff_len = strlen(buff);
 	char *str;
 
 	readchar = read(fd, stream, READ_SIZE);
